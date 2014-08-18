f3f896d48f701aab17fd8c1d2bdd9e222ce374e0
kzviagina <apostatta@yandex.ru>
2014-08-19 02:50:16 +0300

    Added company constructor, methods

--- 09-prototype/script.js
+++ 09-prototype/script.js
@@ -8,6 +8,57 @@ function extend(Child, Parent) {
 }
 
 
+//Company
+function Company(name, director) {
+    var ceo = director;
+    this.name = name;
+    this.employees = {
+        cleaners: [],
+        guards: [],
+        trainees: [],
+        developers: [],
+        managers: []
+    };
+    this.getArray = function (person) {
+        var companyArr = [];
+        switch (person.type) {
+            case 'cleaner':
+                companyArr = this.employees.cleaners;
+                break;
+            case 'guard':
+                companyArr = this.employees.guards;
+                break;
+            case 'trainee':
+                companyArr = this.employees.trainees;
+                break;
+            case 'developer':
+                companyArr = this.employees.developers;
+                break;
+            case 'manager':
+                companyArr = this.employees.managers;
+                break;
+            default:
+                console.log(person.name + ' Sorry! There are no such positions in ' + this.name + '!');
+        }
+        return companyArr;
+    };
+    this.hire = function (initiator, person) {
+        var companyArr = this.getArray(person);
+        companyArr.push(person);
+        console.log(person.name + ', welcome aboard to ' + this.name);
+    };
+    this.fire = function (initiator, person) {
+        if (initiator === ceo || initiator === person) {
+            var companyArr = this.getArray(person),
+                idx = companyArr.indexOf(person);
+            companyArr.splice(idx, 1);
+            console.log(person.name + ' you are fired!11');
+        } else {
+            console.log(initiator.name + ' hasn\'t enough rules to fire ' + person.name);
+        }
+    };
+}
+
 //Employee classes
 //name - String
 //age - Number
@@ -37,117 +88,143 @@ Employee.prototype.retire = function () {
 };
 
 //Cleaner
-function Cleaner () {
+function Cleaner() {
     Cleaner.superclass.constructor.apply(this, arguments);
-    this.cleanUp = function () {
-        console.log(this.name + ' started to clean up');
-    };
+    this.type = 'cleaner';
 }
+extend(Cleaner, Employee);
+Cleaner.prototype.cleanUp = function() {
+    console.log(this.name + ' started to clean up');
+};
+
+
 //Guard
 function Guard () {
     Guard.superclass.constructor.apply(this, arguments);
-    this.guardPlace = function () {
-        console.log(this.name + ' started to guard');
-    };
+    this.type = 'guard';
 }
+extend(Guard, Employee);
+Guard.prototype.guardPlace = function() {
+    console.log(this.name + ' started to guard');
+};
+
 
 function Boss () {
     Boss.superclass.constructor.apply(this, arguments);
+    this.type = 'boss';
 }
+extend(Boss, Employee);
+Boss.prototype.toInterview = function (person) {
+    console.log(this.name + ' is interviewing ' + person.name);
+};
+Boss.prototype.hireEmployee = function (company, person) {
+    company.hire(this, person);
+};
 
 //CTO & Manager inherit from Boss
 function CEO () {
     CEO.superclass.constructor.apply(this, arguments);
+    this.type = 'ceo';
 }
+extend(CEO, Boss);
+CEO.prototype.fireEmployee = function (company, person) {
+     company.fire(this, person);
+};
 
-function CTO () {
-    CTO.superclass.constructor.apply(this, arguments);
-}
 
+//Manager
 function Manager () {
     Manager.superclass.constructor.apply(this, arguments);
-    this.assignTask = function (developer, taskName) {
-        developer.acceptTask(taskName);
-    };
+    this.type = 'manager';
 }
+extend(Manager, Boss);
+Manager.prototype.assignTask = function(developer, taskName) {
+    developer.receiveTask(taskName);
+};
+
 //Developer
 function Developer () {
     Developer.superclass.constructor.apply(this, arguments);
-    var tasks = [],
-        codeLines = 0;
+    //private property
+    var tasks = [];
+    this.codeLines = 0;
+    this.type = 'developer';
     this.acceptTask = function (taskName) {
-        if (!tasks.length) {
-            tasks.push(taskName);
-        } else {
-            console.log('Sorry, I have tasks already');
-        }       
-        
-            console.log('tasks ' + tasks);
+        tasks.push(taskName);
+        console.log(this.name + ' took task ' + taskName);
     };
-    this.toCode = function () {
-        if (tasks.length) {
-            console.log('And so you code...');
-            codeLines += 2;
-            console.log('codeLines ' + codeLines);
-        }
-    };
-    this.fixBugs = function () {
-        if (codeLines > 0) {
-            console.log('Compile and load... In debug mode');
-            codeLines--;
-            console.log('codeLines ' + codeLines);
-        } else {
-            console.log('No bugs to fix!');
-        }
+    this.hasTasks = function () {
+        return (tasks.length > 0);
     };
 }
+extend(Developer, Employee);
+Developer.prototype.receiveTask = function(taskName) {
+    if (!this.hasTasks()) {
+        this.acceptTask(taskName);
+    } else {
+        console.log(this.name + ': Sorry, I have tasks already');
+    }
+};
+Developer.prototype.toCode = function() {
+    if (this.hasTasks()) {
+        console.log('And so you code...');
+        this.codeLines += 2;
+    } else {
+        console.log(this.name + ': I can\'t code cause I have no tasks!');
+    }
+};
+Developer.prototype.fixBugs = function() {
+    if (this.codeLines > 0) {
+        console.log('Compile and load... In debug mode');
+        this.codeLines--;
+    } else {
+        console.log('No bugs to fix!');
+    }
+};
 
-//Trainee inherits from Developer do not receive salary
+
+//Trainee inherits from Developer but do not receive salary
 function Trainee() {
     Trainee.superclass.constructor.apply(this, arguments);
-    this.receiveSalary = function() {
-        console.log(this.name + ' is trainee and can\'t receive salary');
-    };
+    this.type = 'trainee';
 }
-
-//Inheritance
-extend(Cleaner, Employee);
-extend(Guard, Employee);
-extend(Boss, Employee);
-extend(CEO, Boss);
-extend(CTO, Boss);
-extend(Manager, Boss);
-extend(Developer, Employee);
 extend(Trainee, Developer);
+Trainee.prototype.receiveSalary = function() {
+    console.log(this.name + ' is trainee and can\'t receive salary');
+};
 
-var me = new Developer('Kate', 26, 0);
-var he = new Trainee('Serg', 24, 1);
-var mng = new Manager('Katya', 22, 0);
-mng.assignTask(me, 'First task');
-mng.assignTask(me, 'Second task');
+
+//Sad story
+var kate = new Developer('Kate', 26, 0),
+    trainee = new Trainee('Sergey', 24, 1),
+    manager = new Manager('Ilona', 29, 0),
+    director = new CEO('Eugene', 42, 1),
+    sigma = new Company ('Sigma', director);
+
+director.hireEmployee(sigma, manager);
+manager.toInterview(kate);
+director.toInterview(kate);
+manager.hireEmployee(sigma, kate);
+manager.toInterview(trainee);
+manager.hireEmployee(sigma, trainee);
+
+kate.comeToWork();
+trainee.comeToWork();
+
+manager.assignTask(kate, 'First task');
+manager.assignTask(kate, 'Second task');
+
 for (var i = 0; i < 3; i++) {
-    me.toCode();
-}
-for (var i = 0; i < 7; i++) {
-    me.fixBugs();
+    kate.toCode();
 }
 
-//Company
-function Company () {
-    this.employees = {};
-    this.cleaners = [];
-    this.guards = [];
-    this.trainees = [];
-    this.qa = [];
-    this.developers = [];
-    this.managers = [];
-    this.directors = [];
-        
-    this.hireEmployee = function (person) {
-        //company.push(person);
-    };
-    this.fireEmployee = function (person) {
-        //var idx = person.indexOf(companyArr);
-        //companyArr.splice(idx, 1);
-    };
+for (var i = 0; i < 7; i++) {
+    kate.fixBugs();
 }
+
+director.fireEmployee(sigma, kate);
+
+trainee.toCode();
+manager.assignTask(trainee, 'Second task');
+trainee.toCode();
+trainee.fixBugs();
